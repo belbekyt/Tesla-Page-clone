cbdabea013556293714901d79c93c3f8b8f92f79
Miłosz Pawłowski <milosz.pawlowski.torun@wp.pl>
2022-09-03 14:59:13 +0200

    Add files via upload

--- js/menu-aside.js
+++ js/menu-aside.js
@@ -2,13 +2,33 @@ const aside = $("#aside-menu");
 const menuBtn = $("#menu-btn");
 const close = $(".close-btn");
 const body = $("body");
+const specialMenu = $("#special-menu");
+const normalMenu = $("#normal-menu");
+
+specialMenu.hide();
 
 menuBtn.click(() => {
     aside.show(600);
-    // body.addClass("item-blur");
-    aside.addClass("item-not-blur");
 })
 
 close.click(() => {
     aside.hide(500);
 })
+
+let windowWidth = 0;
+
+const checkWidth = () => {
+    if(windowWidth<1200){
+        specialMenu.show();
+        normalMenu.hide();
+    }
+    else{
+        specialMenu.hide();
+        normalMenu.show();
+    }
+}
+
+window.addEventListener("resize", () => {
+    windowWidth = window.innerWidth;
+    checkWidth();
+});
